5fe9c1b5210c277410049d15a61cb17243ca4083
seima <linuxseima@gmail.com>
2021-10-23 11:51:01 +0900

    add vueMeta

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import $router from 'vue-router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueHead from 'vue-head'
+import VueMeta from 'vue-meta'
 
 Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios, $router)
 Vue.use(VueHead)
+Vue.use(VueMeta)
 
 new Vue({
   router,
