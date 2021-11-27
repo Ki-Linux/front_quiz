cb97d51cc00780f90d9e96ed0d2086a80b6cb562
seima <linuxseima@gmail.com>
2021-11-27 18:30:54 +0900

    add name password

--- src/views/add.vue
+++ src/views/add.vue
@@ -23,17 +23,45 @@
 
         public name = '';
         public password = "";
+        public Written = false;
 
         setup(): void {
             console.log('set up can');
             
             axios.defaults.baseURL = "http://localhost:3000";
 
-            axios.post('/post/namePost', {
+            if(this.name !== "" && this.password !== "") {
+
+                console.log("no nothing Written");
+                this.Written = true;
+
+            } else if(this.name === "" && this.password === "") {
+
+                console.log("nothing both");
+
+            } else if(this.name === "") {
+
+                console.log("nothing name");
+
+            } else if(this.password === "") {
+
+                console.log("nothing password");
+
+            }
+
+            const canPost = () => {
+
+                axios.post('/post/namePost', {
                 postName: this.name,
                 postPassword: this.password
 
-            });
+                });
+            }
+
+            if(this.Written === true) {
+                canPost();
+            }
+            
 
         }
     }
