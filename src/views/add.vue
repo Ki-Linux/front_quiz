c5cc0e1b158bb3583995b19a77f4c3999a410ad0
seima <linuxseima@gmail.com>
2021-11-27 15:36:26 +0900

    add name and password

--- src/views/add.vue
+++ src/views/add.vue
@@ -3,11 +3,11 @@
         <h1>新規登録</h1>
         <div class="user">
             <h2>ユーザーニックネーム</h2>
-            <input type="text">
+            <input type="text" v-model="name">
         </div>
         <div class="password">
             <h2>パスワード</h2>
-            <input type="text">
+            <input type="text" v-model="password">
         </div>
         <div class="setup">
             <button @click="setup">登録する</button>
@@ -16,11 +16,30 @@
 </template>
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
+    import axios from 'axios'
 
     @Component
     export default class add extends Vue {
+
+        public name = '';
+        public password = "";
+
         setup(): void {
             console.log('set up can');
+            
+            axios.defaults.baseURL = "http://localhost:3000";
+
+            axios.post('/post/namePost', {
+                postName: this.name,
+                postPasswrod: this.password
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
         }
     }
 </script>
