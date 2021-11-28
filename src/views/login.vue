ef1e8fbee440aad8e264366aceef85451c2cedd2
seima <linuxseima@gmail.com>
2021-11-28 17:56:05 +0900

    add login

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,8 +1,30 @@
 <template>
     <div id="login">
-
+        <h1>ログイン</h1>
+        <my-edit-login @to-parent="toParent"/>
+        <div class="setup">
+            <p>{{ showNothing }}</p>
+            <button @click="setup">ログインする</button>
+        </div>
     </div>
 </template>
-<script>
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import myEdit from '@/components/myEdit.vue';
 
+    @Component({
+        components: {
+            'my-edit-login': myEdit,
+        }
+    })
+    export default class login extends Vue {
+
+        public login_name = "";
+        public login_password = "";
+
+        toParent(childName: string, childPassword: string): void {
+            this.login_name = childName;
+            this.login_password = childPassword;
+        }
+    }
 </script>
