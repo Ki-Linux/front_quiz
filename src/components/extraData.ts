23f35671311ac21f11710634878c68709c0fa2a1
seima <linuxseima@gmail.com>
2021-12-02 18:53:31 +0900

    add extraData

---
+++ src/components/extraData.ts
@@ -0,0 +1,142 @@
+const questionExtra: string[] = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j'
+]    
+
+const answerExtra: { select: string; judge: boolean; }[][] = [
+    
+    [
+        {
+            select:'a1',
+            judge: true
+        },
+        {
+            select:'b1',
+            judge: true
+        },
+        {
+            select:'c1',
+            judge: true
+        },
+        {
+            select:'d1',
+            judge: true
+        },
+        {
+            select:'e1',
+            judge: true
+        },
+        {
+            select:'f1',
+            judge: true
+        },
+        {
+            select:'g1',
+            judge: true
+        },
+        {
+            select:'h1',
+            judge: true
+        },
+        {
+            select:'i1',
+            judge: true
+        },
+        {
+            select:'j1',
+            judge: true
+        }
+    ],
+    [
+        {
+            select:'a2',
+            judge: true
+        },
+        {
+            select:'b2',
+            judge: true
+        },
+        {
+            select:'c2',
+            judge: true
+        },
+        {
+            select:'d2',
+            judge: true
+        },
+        {
+            select:'e2',
+            judge: true
+        },
+        {
+            select:'f2',
+            judge: true
+        },
+        {
+            select:'g2',
+            judge: true
+        },
+        {
+            select:'h2',
+            judge: true
+        },
+        {
+            select:'i2',
+            judge: true
+        },
+        {
+            select:'j2',
+            judge: true
+        }       
+    ],
+    [
+        {
+            select:'a3',
+            judge: true
+        },
+        {
+            select:'b3',
+            judge: true
+        },
+        {
+            select:'c3',
+            judge: true
+        },
+        {
+            select:'d3',
+            judge: true
+        },
+        {
+            select:'e3',
+            judge: true
+        },
+        {
+            select:'f3',
+            judge: true
+        },
+        {
+            select:'g3',
+            judge: true
+        },
+        {
+            select:'h3',
+            judge: true
+        },
+        {
+            select:'i3',
+            judge: true
+        },
+        {
+            select:'j3',
+            judge: true
+        }  
+    ]
+]
