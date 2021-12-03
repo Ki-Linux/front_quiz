dc7cd792a7768a631cf757699a07686650a9402a
seima <linuxseima@gmail.com>
2021-12-03 22:15:55 +0900

    add axios

---
+++ src/components/userNameFromServer.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+let showNameFromServer
+
+const sendUserName = () => {
+
+    axios.get('/getUserNameWithiykrnmltpoebrlmknebwr34t35reefwefWEFYUMm4te')
+    .then((response) => {
+         showNameFromServer = response.data;
+         console.log(showNameFromServer);
+    })
+    .catch((err) => {
+       console.log(err);
+    })
+}
+                
+export { sendUserName }  
