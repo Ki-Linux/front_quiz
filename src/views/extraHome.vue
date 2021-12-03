c56c45f3e2a748843db861dce7d012db3fa39fb2
seima <linuxseima@gmail.com>
2021-12-03 18:53:51 +0900

    can extraHome

--- src/views/extraHome.vue
+++ src/views/extraHome.vue
@@ -1,4 +1,30 @@
 <template>
-   <p>UI</p> 
+   <div id="extraHome">
+      <p>extra</p>
+      <p>{{ showUserName }}</p>
+   </div>
 </template>
+<script lang="ts">
+   import { Component, Vue } from 'vue-property-decorator';
+   import axios from 'axios';
 
+   @Component
+   export default class extraHome extends Vue {
+      public showUserName = "";
+
+   mounted() {
+
+      axios.defaults.baseURL = "http://localhost:3000";
+
+      axios.get('/getUserNameWithiykrnmltpoebrlmknebwr34t35reefwefWEFYUMm4te')
+      .then((response) => {
+         this.showUserName = response.data;
+      })
+      .catch((err) => {
+         console.log(err);
+      })
+
+      }
+
+   }
+</script>
