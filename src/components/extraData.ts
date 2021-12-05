43f0277ec52f9b5385444b4dd491081e4c6521e1
seima <linuxseima@gmail.com>
2021-12-05 13:24:36 +0900

    ad false

--- src/components/extraData.ts
+++ src/components/extraData.ts
@@ -8,7 +8,27 @@ const questionExtra: string[] = [
     'g',
     'h',
     'i',
-    'j'
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'a',
+    'b',
+    'c',
+    'd'
 ]    
 
 const answerExtra: { select: string; judge: boolean; }[][] = [
@@ -53,51 +73,315 @@ const answerExtra: { select: string; judge: boolean; }[][] = [
         {
             select:'j1',
             judge: true
+        },
+        {
+            select:'k1',
+            judge: false
+        },
+        {
+            select:'l1',
+            judge: false
+        },
+        {
+            select:'m1',
+            judge: false
+        },
+        {
+            select:'n1',
+            judge: false
+        },
+        {
+            select:'o1',
+            judge: false
+        },
+        {
+            select:'p1',
+            judge: false
+        },
+        {
+            select:'q1',
+            judge: false
+        },
+        {
+            select:'r1',
+            judge: false
+        },
+        {
+            select:'s1',
+            judge: false
+        },
+        {
+            select:'t1',
+            judge: false
+        },
+        {
+            select:'u1',
+            judge: false
+        },
+        {
+            select:'v1',
+            judge: false
+        },
+        {
+            select:'w1',
+            judge: false
+        },
+        {
+            select:'x1',
+            judge: false
+        },
+        {
+            select:'y1',
+            judge: false
+        },
+        {
+            select:'z1',
+            judge: false
+        },
+        {
+            select:'a1',
+            judge: false
+        },
+        {
+            select:'b1',
+            judge: false
+        },
+        {
+            select:'c1',
+            judge: false
+        },
+        {
+            select:'d1',
+            judge: false
         }
     ],
     [
         {
             select:'a2',
-            judge: true
+            judge: false
         },
         {
             select:'b2',
-            judge: true
+            judge: false
         },
         {
             select:'c2',
-            judge: true
+            judge: false
         },
         {
             select:'d2',
-            judge: true
+            judge: false
         },
         {
             select:'e2',
-            judge: true
+            judge: false
         },
         {
             select:'f2',
-            judge: true
+            judge: false
         },
         {
             select:'g2',
-            judge: true
+            judge: false
         },
         {
             select:'h2',
-            judge: true
+            judge: false
         },
         {
             select:'i2',
-            judge: true
+            judge: false
         },
         {
             select:'j2',
+            judge: false
+        },
+        {
+            select:'k2',
             judge: true
+        },
+        {
+            select:'l2',
+            judge: true
+        },
+        {
+            select:'m2',
+            judge: true
+        },
+        {
+            select:'n2',
+            judge: true
+        },
+        {
+            select:'o2',
+            judge: true
+        },
+        {
+            select:'p2',
+            judge: true
+        },
+        {
+            select:'q2',
+            judge: true
+        },
+        {
+            select:'r2',
+            judge: true
+        },
+        {
+            select:'s2',
+            judge: true
+        },
+        {
+            select:'t2',
+            judge: true
+        },
+        {
+            select:'u2',
+            judge: false
+        },
+        {
+            select:'v2',
+            judge: false
+        },
+        {
+            select:'w2',
+            judge: false
+        },
+        {
+            select:'x2',
+            judge: false
+        },
+        {
+            select:'y2',
+            judge: false
+        },
+        {
+            select:'z2',
+            judge: false
+        },
+        {
+            select:'a2',
+            judge: false
+        },
+        {
+            select:'b2',
+            judge: false
+        },
+        {
+            select:'c2',
+            judge: false
+        },
+        {
+            select:'d2',
+            judge: false
         }       
     ],
     [
+        {
+            select:'a3',
+            judge: false
+        },
+        {
+            select:'b3',
+            judge: false
+        },
+        {
+            select:'c3',
+            judge: false
+        },
+        {
+            select:'d3',
+            judge: false
+        },
+        {
+            select:'e3',
+            judge: false
+        },
+        {
+            select:'f3',
+            judge: false
+        },
+        {
+            select:'g3',
+            judge: false
+        },
+        {
+            select:'h3',
+            judge: false
+        },
+        {
+            select:'i3',
+            judge: false
+        },
+        {
+            select:'j3',
+            judge: false
+        },
+        {
+            select:'k3',
+            judge: false
+        },
+        {
+            select:'l3',
+            judge: false
+        },
+        {
+            select:'m3',
+            judge: false
+        },
+        {
+            select:'n3',
+            judge: false
+        },
+        {
+            select:'o3',
+            judge: false
+        },
+        {
+            select:'p3',
+            judge: false
+        },
+        {
+            select:'q3',
+            judge: false
+        },
+        {
+            select:'r3',
+            judge: false
+        },
+        {
+            select:'s3',
+            judge: false
+        },
+        {
+            select:'t3',
+            judge: false
+        },
+        {
+            select:'u3',
+            judge: true
+        },
+        {
+            select:'v3',
+            judge: true
+        },
+        {
+            select:'w3',
+            judge: true
+        },
+        {
+            select:'x3',
+            judge: true
+        },
+        {
+            select:'y3',
+            judge: true
+        },
+        {
+            select:'z3',
+            judge: true
+        },
         {
             select:'a3',
             judge: true
@@ -113,31 +397,7 @@ const answerExtra: { select: string; judge: boolean; }[][] = [
         {
             select:'d3',
             judge: true
-        },
-        {
-            select:'e3',
-            judge: true
-        },
-        {
-            select:'f3',
-            judge: true
-        },
-        {
-            select:'g3',
-            judge: true
-        },
-        {
-            select:'h3',
-            judge: true
-        },
-        {
-            select:'i3',
-            judge: true
-        },
-        {
-            select:'j3',
-            judge: true
-        }  
+        }     
     ]
 ]
 
