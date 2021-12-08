bab98f58eb63f02d4669079295aa4bdd469df409
seima <linuxseima@gmail.com>
2021-12-08 23:21:27 +0900

    add img 2

--- src/components/extraImgData.ts
+++ src/components/extraImgData.ts
@@ -22,17 +22,101 @@ const answerExtra: { select: HTMLImageElement; judge: boolean; }[][] = [
         {
             select: require("@/assets/hawaiiTrue.png"),
             judge: true
+        },
+        {
+            select: require("@/assets/imori.png"),
+            judge: true
+        },
+        {
+            select: require("@/assets/race.png"),
+            judge: true
+        },
+        {
+            select: require("@/assets/pyoko.png"),
+            judge: true
+        },
+        {
+            select: require("@/assets/kutwurufu.png"),
+            judge: true
+        },
+        {
+            select: require("@/assets/hakka.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/karuizawa.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/suisu.png"),
+            judge: false
         }
     ],
     [
         {
             select: require("@/assets/hawaiiFalse2.png"),
             judge: false
+        },
+        {
+            select: require("@/assets/kame.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/nou.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/sanji.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/monster.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/kurakka.png"),
+            judge: true
+        },
+        {
+            select: require("@/assets/kyoto.png"),
+            judge: true
+        },
+        {
+            select: require("@/assets/japan.png"),
+            judge: true
         }
     ],
     [
         {
             select: require("@/assets/hawaiiFalse1.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/same.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/saiya.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/fugu.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/chuunibyou.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/cookie.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/miyajima.png"),
+            judge: false
+        },
+        {
+            select: require("@/assets/igirisu.png"),
             judge: false
         }
     ]
