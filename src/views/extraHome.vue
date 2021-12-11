9e35f0e15d3bba209728bf1bff6c9ed8a6112cbb
seima <linuxseima@gmail.com>
2021-12-11 14:58:30 +0900

    extraHome button

--- src/views/extraHome.vue
+++ src/views/extraHome.vue
@@ -1,23 +1,23 @@
 <template>
    <div id="extraHome">
       <div class="top">
-         <ul>
-            <li><router-link to="/">戻る</router-link></li>
-            <li>ようこそ{{ showUserName }}様</li>
-         </ul>
+            <!-- <li><router-link to="/">戻る</router-link></li> -->
+            <p>ようこそ{{ showUserName }}様</p>
       </div>
-      <h1>エキストラステージ</h1>
       <div class="buttonAndAverage">
+            <h1>エキストラステージ</h1>
          <div class="buttonToExtra">
             <button @click="toQuiz">エキストラ</button>
          </div>
          <ul>
+            <li>平均正答率</li>
             <li>{{ average }}%</li>
+            <li><meter id="yu" min="0" max="100" :value="average">max100%</meter></li>
             <li>{{ individual }}</li>
          </ul>
-         <meter id="yu" min="0" max="100" :value="average">max100%</meter>
+
       </div>
-      <div class="quizOFIMG">
+      <div class="quizOfImg">
          <div class="imgDes">
             <h1>画像クイズ</h1>
             <p>画像クイズは全15問のうち5問がランダムで出題されます。</p>
@@ -26,10 +26,11 @@
             <button @click="toImgQuiz">画像クイズ</button>
          </div>
          <ul>
+            <li>平均正答率</li>
             <li>{{ averageImg }}%</li>
+            <li><meter id="yu" min="0" max="100" :value="average">max100%</meter></li>
             <li>{{ individual }}</li>
          </ul>
-         <meter id="yu" min="0" max="100" :value="average">max100%</meter>
       </div>
    </div>
 </template>
@@ -150,3 +151,71 @@
 
    }
 </script>
+<style scoped lang="scss">
+
+$breakpoint: (
+  sp: 'screen and (max-width: 480px)'
+);
+
+@mixin sp {
+  @media #{map-get($breakpoint, sp)} {
+    @content;
+  }
+}
+
+@include sp {
+   .imgDes p {
+      padding: 0 20px;
+   }
+}
+
+
+#extraHome {
+   .top > p {
+         font-size: 20px;
+         float: right;
+         padding: 20px 20px 0 0;
+   }
+
+
+$blue: rgb(3, 3, 116);
+   .buttonAndAverage {
+
+         h1 {
+            padding-top: 70px;
+         }
+
+          button, ul > li {
+         &:first-of-type, &:nth-of-type(2) {
+            color: $blue;
+         }
+      }
+   }
+
+
+$orange: rgb(116, 63, 3);
+.quizOfImg {
+   padding-top: 60px;
+
+      button, ul > li {
+      &:first-of-type, &:nth-of-type(2) {
+         color: $orange;
+      }
+   }
+}
+
+button {
+      font-size: 45px;
+      margin-top: 20px;
+      padding: 7px 19px;
+      background-color: rgba(234, 241, 234, 0.678);
+   }
+
+   ul > li {
+   &:first-of-type, &:nth-of-type(2), &:nth-of-type(3){
+      font-size: 30px;
+   }
+      font-size: 18px;
+   }
+}
+</style>
