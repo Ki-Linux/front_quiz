dcc671d4fcd7874b9bffa00d00648af42c2f7662
seima <linuxseima@gmail.com>
2021-12-11 20:39:10 +0900

    add honnnyou

--- src/views/add.vue
+++ src/views/add.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="add">
         <h1>新規登録</h1>
-        <p>新規登録をすると<br>エキストラクイズ、画像クイズができます。</p>
+        <p>新規登録をすると<br>エキストラクイズ、画像クイズができます。<br>※本名での登録はお控えください。</p>
         <my-edit @to-parent="toParent"/>
         <div class="setup">
             <p>{{ showNothing }}</p>
